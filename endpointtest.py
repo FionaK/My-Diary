dcd30befe5c22d576e9dc6776a6ac889dd1e447d
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-18 19:03:58 +0300

    run test

--- endpointtest.py
+++ endpointtest.py
@@ -20,13 +20,20 @@ class EndpointsTestCase (unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	#test create_entry
-	"""def test_create_entry(self):
+	def test_create_entry(self):
 		tester = app.test_client(self)
 		response =tester.get('/api/v1/create_entry/', content_type='application/json'(
 			entries.update({len(entries)+1:{happy thouthts:happiness can be addictive}})
 			))
 
 		self.assertEqual(response.status_code, 200)
+
+	#test display_entry
+	def test_display_entry(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/display_entry/', content_type='application/json')
+		self.assertEqual(response.status_code, 200)
+
 
 
 
