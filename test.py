d63e3f24b80ce0269df0810706d75d9fafe4721d
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-20 13:04:49 +0300

    add test file

--- test.py
+++ test.py
@@ -6,53 +6,57 @@ import json
 class EndpointsTestCase (unittest.TestCase):
 	#test home message
 	def test_home(self):
-		tester = app.test_client(self)
-		response = tester.get('/api/v1/', content_type='application/json')
-		self.assertEqual(response.status_code, 200)
+		with app.test_client(self) as tester:
+			response = tester.get('/api/v1/', content_type='application/json')
+			self.assertEqual(response.status_code, 200)
 
 	#test registration
 	def test_register(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/register/', content_type='application/json'(
-			details.update({username:{"name":fifi,"email":fko.com,"pass_wd":1234}})
-			))
+		response = tester.get('/api/v1/register/', content_type='application/json')
 
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, 400)
 
 	#test create_entry
 	def test_create_entry(self):
 		tester = app.test_client(self)
-		response =tester.get('/api/v1/create_entry/', content_type='application/json'(
-			entries.update({len(entries)+1:{title:entry}})
-			))
+		response =tester.get('/api/v1/create_entry/', content_type='application/json')
+			
 
-		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_code, 400)
 
 	#test display_entry
 	def test_display_entry(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/display_entry/', methods=['GET'])
-		self.assertEqual(response.status_code, 200)
+		
+		self.assertEqual(tester.get('/api/v1/display_entry/').status_code, 200)
 
 
 	#test delete entry
 	def test_delete_entry(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/delete_entry/', METHODS=['POST'])
+		response = tester.get('/api/v1/delete_entry/4')
 		self.assertEqual(response.status_code, 405)
 
 	#test modify entry
 	def test_modify_entry(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/modify_entry/', content_typr='application/json')
-		self.assertEqual(response.status_code, 200)
+		response = tester.get('/api/v1/modify_entry/')
+		self.assertEqual(response.status_code, 404)
 
 
 	#test get user details
 	def test_get_user(self):
 		tester = app.test_client(self)
-		response = tester.get('/api/v1/modify_entry/')
+		response = tester.get('/api/v1/get_user/')
 		self.assertEqual(response.status_code, 200)
+
+	# test login
+	def test_login(self):
+		tester= app.test_client(self)
+		response = tester.get('/api/v1/login/')
+		self.assertEqual(response.status_code, 500)	
+		self.assertEqual(tester.post('/api/v1/login/', json={"username":"fifi","password":"2345"}).status_code, 200)
 
 if __name__	== '__main__':
 	app.run(debug=True)
