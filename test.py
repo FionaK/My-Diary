d231f7ceae3ae25802a85f528ebb4716b10b9b92
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-20 12:08:19 +0300

    add test file

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from endpoints import *
+from d_structure import *
 import json
 
 
@@ -34,10 +34,37 @@ class EndpointsTestCase (unittest.TestCase):
 		response = tester.get('/api/v1/display_entry/', content_type='application/json')
 		self.assertEqual(response.status_code, 200)
 
+
 	#test delete entry
 	def test_delete_entry(self):
 		tester = app.test_client(self)
-		response =
+		response = tester.get('/api/v1/delete_entry/', content_type='application/json')
+		self.assertEqual(response.status_code, 200)
+
+	#test modify entry
+	def test_modify_entry(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/modify_entry/', content_typr='application/json')
+		self.assertEqual(response.status_code, 200)
+
+
+	#test get user details
+	def test_get_user(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/modify_entry/', content_typr='application/json')
+		self.assertEqual(response.status_code, 200)
+
+if __name__	== '__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
+
+             
 
 
 
