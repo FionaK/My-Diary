77c776c54a422cf22c871b5c59d8c9234b4489f6
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-24 09:50:01 +0300

    api endpoints

---
+++ diary.py
@@ -0,0 +1,117 @@
+from flask import Flask, jsonify, request
+from functools import wraps
+from models import *
+import psycopg2
+import jwt
+
+app=Flask(__name__)
+
+
+@app.route('/api/v1/', methods= ['POST','GET'])
+def home():
+	return jsonify({'message': 'Welcome to my Diary'})
+
+@app.route('/api/v1/register/', methods= ['POST', 'GET'])
+def register():
+	name=request.get_json()['name']
+	username=request.get_json()['username']
+	password=request.get_json()['password']
+	email=request.get_json()['email']
+
+	cur.execute("SELECT * FROM users WHERE username = '"+username+"'")
+	if cur.fetchone() is None:
+		cur.execute("INSERT INTO users(name,username,password,email)VALUES(%s, %s, %s, %s);",(name, username, password, email))
+	else:
+		return jsonify({'message': 'username already exists'}), 409
+	conn.commit()
+
+	return jsonify({'message': 'You are successfully registered'})
+
+@app.route('/api/v1/login/', methods= ['POST', 'GET'])
+def login():
+	username=request.get_json()['username']
+	password=request.get_json()['password']
+	cur.execute("SELECT COUNT(1) FROM users WHERE username = '"+username+"'")
+	if cur.fetchone() is not None:
+		cur.execute("SELECT * FROM users WHERE username = '"+username+"'")
+		for row in cur.fetchall():
+			if password == row[3]:
+				return jsonify({'message': 'Login successful'}), 200
+			else:
+				return jsonify({'message':'wrong password'}), 401
+	else:
+		return jsonify({'message':'Invalid username'}), 401
+
+	conn.commit()
+
+
+@app.route('/api/v1/create_entry/', methods= ['POST', 'GET'])
+def create_entry():
+	title=request.get_json()['title']
+	entry=request.get_json()['entry']
+	username=request.get_json()['username']
+	cur.execute("INSERT INTO entries(title,entry,username)VALUES(%s, %s, %s);",(title, entry, username))
+	conn.commit()
+	return jsonify({'message': 'New entry has been created'}), 200
+
+@app.route('/api/v1/display_entry/', methods= ['GET'])
+def display_entry():
+	cur.execute("SELECT * FROM entries")
+	entries=cur.fetchall() 
+
+	return jsonify(entries), 200
+
+@app.route('/api/v1/single_entry/<int:entryid>', methods= ['GET'])
+def single_entry(entryid):
+	cur.execute("SELECT * FROM entries WHERE entryid="+str(entryid)+";")
+	entry=cur.fetchone()
+	conn.commit()
+
+	return jsonify(entry)
+
+@app.route('/api/v1/delete_entry/<int:entryid>', methods= ['DELETE'])
+def delete_entry(entryid):
+	cur.execute("SELECT * FROM entries WHERE entryid = '"+str(entryid)+"'")
+	user=cur.fetchone()
+	if user is not None:
+		cur.execute("DELETE FROM entries WHERE entryid='"+str(entryid)+"'")
+	else:
+		return jsonify({'message':'No user with such an entry exists'}), 401
+	conn.commit()
+
+	return jsonify({'message': 'Your entry has been successfully deleted'})
+
+@app.route('/api/v1/modify_entry/<int:entryid>', methods= ['PUT'])	
+def modify_entry():
+	title=request.get_json()['title']
+	entry=request.get_json()['entry']
+	username=request.get_json()['username']
+	cur.execute("UPDATE entries SET username WHERE entryid='"+str(entryid)+"'")
+	conn.commit()
+	return jsonify({'message': 'Your entry has been modified'})
+
+@app.route('/api/v1/get_user/', methods=['GET'])
+def get_user():
+	cur.execute("SELECT * FROM users")
+	users=cur.fetchall()
+	return jsonify(users), 200
+
+@app.route('/api/v1/logout/', methods=['GET'])
+def logout():
+	return jsonify({'message': 'Successfully logged out'})
+
+
+if __name__	== '__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
