ba238c2e728e8c03d3881afb0013e94b43db0889
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-25 16:21:30 +0300

    add endpoints

--- app/users/views.py
+++ app/users/views.py
@@ -1,10 +1,32 @@
 from flask import Flask, Blueprint, jsonify, request
+from models import *
+from functools import wraps
+import datetime
+import psycopg2
+import jwt
+from __init__ import *
 
-users_blueprint=Blueprint('users', __name__) 
 
-#class Users():
+users_blueprint=Blueprint('users', __name__)
 
-users_blueprint.route('/api/v2/register/', methods= ['POST', 'GET'])
+
+def token_needed(k):
+	@wraps(k)
+	def decorated(*args, **kwargs):
+		token = request.args.get('access_token')
+		if not token:
+			return jsonify({'message' : 'Missing Token'}), 403
+			try:
+				data = jwt.decode(token, 'fifi')
+			except:
+				return jsonify({'message' : 'Invalid Token'}), 403
+		return k(*args, **kwargs)
+		
+	return decorated
+
+
+
+@users_blueprint.route('/api/v2/register/', methods= ['POST', 'GET'])
 def register():
 	name=request.get_json()['name']
 	username=request.get_json()['username']
@@ -20,7 +42,7 @@ def register():
 	conn.commit()
 	return jsonify({'message': 'You are successfully registered'})
 
-users_blueprint.route('/api/v2/login/', methods= ['POST', 'GET'])
+@users_blueprint.route('/api/v2/login/', methods= ['POST', 'GET'])
 def login():
 	username=request.get_json()['username']
 	password=request.get_json()['password']
@@ -30,19 +52,21 @@ def login():
 		cur.execute("SELECT * FROM users WHERE username = '"+username+"'")
 		for row in cur.fetchall():
 			if password == row[3]:
-				return jsonify({'message': 'Login successful'}), 200
+				token= jwt.encode ({'user':username, 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, 'fifi')
+				return jsonify({'message': 'Login successful', 'access_token': token.decode('UTF-8')}), 200
 			else:
 				return jsonify({'message':'wrong password'}), 401
 	else:
 		return jsonify({'message':'Invalid username'}), 401
 
-users_blueprint.route('/api/v2/get_user/', methods=['GET'])
+@users_blueprint.route('/api/v2/get_user/', methods=['GET'])
+@token_needed
 def get_user():
 	cur.execute("SELECT * FROM users")
 	users=cur.fetchall()
 	return jsonify(users), 200
 
-users_blueprint.route('/api/v2/logout/', methods=['GET'])
+@users_blueprint.route('/api/v2/logout/', methods=['GET'])
 def logout():
 	return jsonify({'message': 'Successfully logged out'})
 
