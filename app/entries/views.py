d78e518c5753e10d4522a9ff26a84e08853a8171
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-25 16:23:25 +0300

    add endpoints

--- app/entries/views.py
+++ app/entries/views.py
@@ -1,10 +1,33 @@
 from flask import Flask, Blueprint, jsonify, request
+from models import *
+from functools import wraps
+import datetime
+import psycopg2
+import jwt
+from __init__ import *
+
 
 entries_blueprint = Blueprint('entries', __name__)
 
-#class Entries():
 
-entries_blueprint.route('/api/v2/create_entry/', methods=['POST'])	
+def token_needed(k):
+	@wraps(k)
+	def decorated(*args, **kwargs):
+		token = request.args.get('access_token')
+		if not token:
+			return jsonify({'message' : 'Missing Token'}), 403
+			try:
+				data = jwt.decode(token, 'fifi')
+			except:
+				return jsonify({'message' : 'Invalid Token'}), 403
+		return k(*args, **kwargs)
+		
+	return decorated
+
+
+
+@entries_blueprint.route('/api/v2/create_entry/', methods=['POST'])
+@token_needed
 def create_entry():
 	title=request.get_json()['title']
 	entry=request.get_json()['entry']
@@ -13,21 +36,24 @@ def create_entry():
 	conn.commit()
 	return jsonify({'message': 'New entry has been created'}), 200
 
-entries_blueprint.route('/api/v2/display_entry/', methods= ['GET'])
+@entries_blueprint.route('/api/v2/display_entry/', methods= ['GET'])
+@token_needed
 def display_entry():
 	cur.execute("SELECT * FROM entries")
 	entries=cur.fetchall()
 
 	return jsonify(entries), 200
 
-entries_blueprint.route('/api/v2/single_entry/<int:entryid>', methods= ['GET'])
+@entries_blueprint.route('/api/v2/single_entry/<int:entryid>', methods= ['GET'])
+@token_needed
 def single_entry(entryid):
 	cur.execute("SELECT * FROM entries WHERE entryid="+str(entryid)+";")
 	entry=cur.fetchone()
 	conn.commit()
 	return jsonify(entry), 200
 
-entries_blueprint.route('/api/v2/delete_entry/<int:entryid>', methods= ['DELETE'])
+@entries_blueprint.route('/api/v2/delete_entry/<int:entryid>', methods= ['DELETE'])
+@token_needed
 def delete_entry(entryid):
 	cur.execute("SELECT * FROM entries WHERE entryid = '"+str(entryid)+"'")
 	user=cur.fetchone()
@@ -39,7 +65,8 @@ def delete_entry(entryid):
 	conn.commit()
 	return jsonify({'message': 'Your entry has been successfully deleted'}), 200
 
-entries_blueprint.route('/api/v2/modify_entry/<int:entryid>', methods= ['PUT'])
+@entries_blueprint.route('/api/v2/modify_entry/<int:entryid>', methods= ['PUT'])
+@token_needed
 def modify_entry(entryid):
 	title=request.get_json()['title']
 	entry=request.get_json()['entry']
