dbc22ef239314a7c45170ab34dc803741dd925d3
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-25 16:52:05 +0300

    add endpoints

--- route.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Blueprint, jsonify, request
-
-home_blueprint = Blueprint('home', __name__)
-
-@home_blueprint.route('/api/v2/', methods=['GET'])
-def home_page():
-	return jsonify({'message': 'Welcome to my diary'})
