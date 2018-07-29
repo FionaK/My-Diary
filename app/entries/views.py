a2cf756ea7f50929f6de4a0e4028bf10b4279b45
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-29 21:13:29 +0300

    add api endpoints

--- app/entries/views.py
+++ app/entries/views.py
@@ -8,75 +8,82 @@ from __init__ import *
 
 
 entries_blueprint = Blueprint('entries', __name__)
-
-class Entries():
-	
-
-	def require_auth(k):
-		@wraps(k)
-		def authorization(*args, **kwargs):
-			token = request.args.get('access_token')
-			if not token:
-				return jsonify({'message' : 'Missing Token'}), 403
-				try:
-					data = jwt.decode(token, 'fifi')
-				except:
-					return jsonify({'message' : 'Invalid Token'}), 408
-			return k(*args, **kwargs)
-			
-		return authorization
+def require_auth(k):
+	@wraps(k)
+	def authorization(*args, **kwargs):
+		token = request.args.get('access_token')
+		if not token:
+			return jsonify({'message' : 'Missing Token'}), 403
+		try:
+			data = jwt.decode(token, 'fifi')
+		except:
+			return jsonify({'message' : 'Invalid Token'}), 408
+		return k(*args, **kwargs)
+		
+	return authorization
 
 
 
-	@entries_blueprint.route('/api/v2/create_entry/', methods=['POST'])
-	@require_auth
-	def create_entry():
+@entries_blueprint.route('/api/v2/create_entry/', methods=['POST'])
+@require_auth
+def create_entry():
+	try:
 		title=request.get_json()['title']
 		entry=request.get_json()['entry']
-		username=request.get_json()['username']
+		data = jwt.decode(request.args.get('access_token'), 'fifi')
+		username = data['user']
+
 		cur.execute("INSERT INTO entries(title,entry,username)VALUES(%s, %s, %s);",(title, entry, username))
 		conn.commit()
 		return jsonify({'message': 'New entry has been created'}), 200
+	except KeyError:
+		return jsonify({'message':'Field can not be blank'}), 406
+		
+@entries_blueprint.route('/api/v2/display_entry/', methods= ['GET'])
+@require_auth
+def display_entry():
+	cur.execute("SELECT * FROM entries")
+	entries=cur.fetchall()
 
-	@entries_blueprint.route('/api/v2/display_entry/', methods= ['GET'])
-	@require_auth
-	def display_entry():
-		cur.execute("SELECT * FROM entries")
-		entries=cur.fetchall()
+	return jsonify(entries), 200
 
-		return jsonify(entries), 200
+@entries_blueprint.route('/api/v2/single_entry/<int:entryid>', methods= ['GET'])
+@require_auth
+def single_entry(entryid):
+	cur.execute("SELECT * FROM entries WHERE entryid="+str(entryid)+";")
+	entry=cur.fetchone()
+	conn.commit()
+	return jsonify(entry), 200
 
-	@entries_blueprint.route('/api/v2/single_entry/<int:entryid>', methods= ['GET'])
-	@require_auth
-	def single_entry(entryid):
-		cur.execute("SELECT * FROM entries WHERE entryid="+str(entryid)+";")
-		entry=cur.fetchone()
-		conn.commit()
-		return jsonify(entry), 200
+@entries_blueprint.route('/api/v2/delete_entry/<int:entryid>', methods= ['DELETE'])
+@require_auth
+def delete_entry(entryid):
+	cur.execute("SELECT * FROM entries WHERE entryid = '"+str(entryid)+"'")
+	user=cur.fetchone()
+	if user is not None:
+		cur.execute("DELETE FROM entries WHERE entryid='"+str(entryid)+"'")
+	else:
+		return jsonify({'message':'No user with such an entry exists'}), 401
 
-	@entries_blueprint.route('/api/v2/delete_entry/<int:entryid>', methods= ['DELETE'])
-	@require_auth
-	def delete_entry(entryid):
-		cur.execute("SELECT * FROM entries WHERE entryid = '"+str(entryid)+"'")
-		user=cur.fetchone()
-		if user is not None:
-			cur.execute("DELETE FROM entries WHERE entryid='"+str(entryid)+"'")
-		else:
-			return jsonify({'message':'No user with such an entry exists'}), 401
+	conn.commit()
+	return jsonify({'message': 'Your entry has been successfully deleted'}), 200
 
-		conn.commit()
-		return jsonify({'message': 'Your entry has been successfully deleted'}), 200
-
-	@entries_blueprint.route('/api/v2/modify_entry/<int:entryid>', methods= ['PUT'])
-	@require_auth
-	def modify_entry(entryid):
-		title=request.get_json()['title']
-		entry=request.get_json()['entry']
-		username=request.get_json()['username']
-
-		cur.execute("UPDATE entries SET title= '"+title+"', entry='"+entry+"' WHERE entryid='"+str(entryid)+"'")
-		conn.commit()
-		return jsonify({'message': 'Your entry has been modified'}), 200
+@entries_blueprint.route('/api/v2/modify_entry/<int:entryid>', methods= ['PUT'])
+@require_auth
+def modify_entry(entryid):
+	title=request.get_json()['title']
+	entry=request.get_json()['entry']
+	data = jwt.decode(request.args.get('access_token'), 'fifi')
+	username= data['user']
+	cur.execute("SELECT * FROM entries WHERE username = '"+username+"' AND entryid = '"+str(entryid)+"'")
+	result = cur.fetchone()
+	if result is None:
+		return jsonify({'message':'You are not authorized to modify this entry'})
+		
+	cur.execute("UPDATE entries SET title= '"+title+"', entry='"+entry+"' WHERE entryid='"+str(entryid)+"'")
+	
+	conn.commit()
+	return jsonify({'message': 'Your entry has been modified'}), 200
 
 
 		
--- app/users/views.py
+++ app/users/views.py
@@ -4,83 +4,100 @@ from functools import wraps
 import datetime
 import psycopg2
 import jwt
+import re
+import base64
 from __init__ import *
 
 
 users_blueprint=Blueprint('users', __name__)
 
-class Users():
-	
+    
 
-
-	def require_auth(k):
-		@wraps(k)
-		def authorization(*args, **kwargs):
-			token = request.args.get('access_token')
-			if not token:
-				return jsonify({'message' : 'Missing Token'}), 403
-				try:
-					data = jwt.decode(token, 'fifi')
-				except:
-					return jsonify({'message' : 'Invalid Token'}), 408
-			return k(*args, **kwargs)
-			
-		return authorization
-
-
-
-	@users_blueprint.route('/api/v2/register/', methods= ['POST', 'GET'])
-	def register():
+def require_auth(k):
+	@wraps(k)
+	def authorization(*args, **kwargs):
+		token = request.args.get('access_token')
+		if not token:
+			return jsonify({'message' : 'Missing Token'}), 403
 		try:
-			name=request.get_json()['name']
-			username=request.get_json()['username']
-			password=request.get_json()['password']
-			email=request.get_json()['email']
-			if len(password)<3:
-				return jsonify("password should be 4 characters or more")
-			if len(name)<3:
-				return jsonify({'message': 'name should not be less than 4 characters'})
+			data = jwt.decode(token, 'fifi')
+		except:
+			return jsonify({'message' : 'Invalid Token'}), 408
+		return k(*args, **kwargs)
+		
+	return authorization
 
+def valid_password(password):
+	if not re.match(r'^(?=\S{6,25}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])',password):
+		return jsonify({'message':'False'})
+	else:
+		return jsonify({'message':'True'})	
+
+
+
+@users_blueprint.route('/api/v2/register/', methods= ['POST', 'GET'])
+def register():
+	try:
+		name=request.get_json()['name']
+		username=request.get_json()['username']
+
+		password=base64.b64encode(bytes(request.get_json()['password']))
+		rpt_password=base64.b64encode(bytes(request.get_json()['repeat_password']))
+		email=request.get_json()['email']
+		if len(name)==0 or len(email)==0:
+			return jsonify({'message':'This field can not be empty!'}), 406
+		if len(password)<6:
+			return jsonify({'message':'Try again! Password should be 6 characters or more'}), 406
+		if len(username)<4:
+			return jsonify({'message': 'Try again! Username should be 4 characters or more'}), 406
+		if password != rpt_password:
+			return jsonify({'message':'Password do not match. Try again!'}), 403
+
+		else:
 			cur.execute("SELECT * FROM users WHERE username = '"+username+"' OR email = '"+email+"'")
 			if cur.fetchone() is None:
-				cur.execute("INSERT INTO users(name,username,password,email)VALUES(%s, %s, %s, %s);",(name, username, password, email))
+				if valid_password(password):
+					cur.execute("INSERT INTO users(name,username,password,email)VALUES(%s, %s, %s, %s);",(name, username, password, email))
+				else:
+					return jsonify({'message':'wrong password format'})
+					
 			else:
 				return jsonify({'message': 'username or email already taken'}), 409
-				
+			
 
-			conn.commit()
+		conn.commit()
 
-			return jsonify({'message': 'You are successfully registered'})
-		except KeyError:
-			return jsonify({'message':'Field can not be blank'}), 406
+		return jsonify({'message': 'You are successfully registered'})
+	except KeyError:
+		return jsonify({'message':'Field can not be blank'}), 406
 
-	@users_blueprint.route('/api/v2/login/', methods= ['POST', 'GET'])
-	def login():
-		username=request.get_json()['username']
-		password=request.get_json()['password']
+@users_blueprint.route('/api/v2/login/', methods= ['POST', 'GET'])
+def login():
+	username=request.get_json()['username']
+	password=base64.b64encode(bytes(request.get_json()['password']))
 
-		cur.execute("SELECT COUNT(1) FROM users WHERE username = '"+username+"'")
-		if cur.fetchone() is not None:
-			cur.execute("SELECT * FROM users WHERE username = '"+username+"'")
-			for row in cur.fetchall():
-				if password == row[3]:
-					token= jwt.encode ({'user':username, 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, 'fifi')
-					return jsonify({'message': 'Login successful', 'access_token': token.decode('UTF-8')}), 200
-				else:
-					return jsonify({'message':'wrong password'}), 401
-		else:
-			return jsonify({'message':'Invalid username'}), 401
+	cur.execute("SELECT COUNT(1) FROM users WHERE username = '"+username+"'")
+	if cur.fetchone() is not None:
+		cur.execute("SELECT * FROM users WHERE username = '"+username+"'")
+		for row in cur.fetchall():
+			if password == row[3]:
+				token= jwt.encode ({'user':username, 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, 'fifi')
+				return jsonify({'message': 'Login successful', 'access_token': token.decode('UTF-8')}), 200
+			else:
+				return jsonify({'message':'wrong password'}), 401
+	else:
+		return jsonify({'message':'Invalid username'}), 401
 
-	@users_blueprint.route('/api/v2/get_user/', methods=['GET'])
-	@require_auth
-	def get_user():
-		cur.execute("SELECT * FROM users")
-		users=cur.fetchall()
-		return jsonify(users), 200
+@users_blueprint.route('/api/v2/get_user/', methods=['GET'])
+@require_auth
+def get_user():
+	cur.execute("SELECT * FROM users")
+	users=cur.fetchall()
+	return jsonify(users), 200
 
-	@users_blueprint.route('/api/v2/logout/', methods=['GET'])
-	def logout():
-		return jsonify({'message': 'Successfully logged out'})
+@users_blueprint.route('/api/v2/logout/', methods=['GET'])
+def logout():
+	return jsonify({'message': 'Successfully logged out'})
 
 
 
