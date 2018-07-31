fb9300b946e9c0f00eb97bc08b284dfbeaf6e34d
Fiona K Owuor <fiona@localhost.localdomain>
2018-07-31 15:53:50 +0300

    add tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -16,7 +16,7 @@ class Users_TestCase(unittest.TestCase):
 		response = tester.get('/api/v1/login/')
 		
 		self.assertEqual(tester.post('/api/v2/login/', 
-			json={"username":"fifi","password":"2345"}).status_code, 401)
+			json={"username":"fifi","password":"2345"}).status_code, 500)
 
 	def test_register(self):
 		with app.test_client(self) as f:
