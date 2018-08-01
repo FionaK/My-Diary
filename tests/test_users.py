ad2deadccf9dca9a3d6b92f77897941ba6a63b03
Fiona K Owuor <fiona@localhost.localdomain>
2018-08-01 14:18:42 +0300

    update tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -17,18 +17,33 @@ class Users_TestCase(unittest.TestCase):
 		response =tester.get('/api/v2/login/')
 		self.assertEqual(tester.post('/api/v2/login',
 			json={"username":"fifi","password":"2345"}).status_code, 301)
+	def test_login(self):
+		tester= app.test_client(self)
+		response =tester.get('/api/v2/login/')
+		self.assertEqual(tester.post('/api/v2/login/',
+			json={"username":"fifi","password":""}).status_code, 401)	
+	def test_login(self):
+		tester= app.test_client(self)
+		response =tester.get('/api/v2/login/')
+		self.assertEqual(tester.post('/api/v2/login/',
+			json={"username":"","password":"fifi23456"}).status_code, 401)	
 
 	def test_register(self):
 		with app.test_client(self) as f:
 			response = f.get('/api/v2/register/')
 			self.assertEqual(f.post('/api/v2/register',
-				json={"name":"fiona","username":"fifi","password":"2345","email":"fko@gmail.com"}).status_code, 301)
+				json={"name":"fiona","username":"fifi","password":"2345", "repeat_password": "2345", "email":"fko@gmail.com"}).status_code, 301)
 
 	def test_register(self):
 		with app.test_client(self) as f:
 			response = f.post('/api/v2/register/')
 			self.assertEqual(f.post('/api/v2/register/',
-				json={"name":"","username":"fifi","password":"2345","email":"fko@gmail.com"}).status_code, 406)		
+				json={"name":"","username":"fifi","password":"2345", "repeat_password": "2345","email":"fko@gmail.com"}).status_code, 406)		
+    	def test_register(self):
+		with app.test_client(self) as f:
+			response = f.post('/api/v2/register/')
+			self.assertEqual(f.post('/api/v2/register/',
+				json={"name":"fiona","username":"fifi","password":"2345", "repeat_password": "25","email":"fko@gmail.com"}).status_code, 403)	
 
 	def test_logout(self):
 		res = app.test_client(self)
