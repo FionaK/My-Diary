665fd0880dbff192f69f1b9de8b0e0f67eaca44a
Fiona K Owuor <fiona@localhost.localdomain>
2018-08-01 14:00:43 +0300

    update tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 import os,sys
 sys.path.insert(0, os.path.abspath(".."))
 from app import flask_app as app
@@ -12,26 +13,10 @@ class Users_TestCase(unittest.TestCase):
 
 
 	def test_login(self):
-		test_data = json.dump({
-			"username": "fifi",
-			"password":"fifi2345"})
-		response = self.app.post('api/v2/login/', Content-type='application/json')
-		self.assertEqual(response.status_code, 200)
-
-	def test_login_with_wrong_password(self):
-		test_data = json.dump({
-			"username": "fifi",
-			"password":""})
-		response = self.app.post('api/v2/login/', Content-type='application/json')
-		self.assertEqual(response.status_code, 401)
-	
-	def test_login_with_wrong_username(self):
-		test_data = json.dump({
-			"username": "",
-			"password":"fifi2345"})
-		response = self.app.post('api/v2/login/', Content-type='application/json')
-		self.assertEqual(response.status_code, 401)
-		
+		tester= app.test_client(self)
+		response =tester.get('/api/v2/login/')
+		self.assertEqual(tester.post('/api/v2/login',
+			json={"username":"fifi","password":"2345"}).status_code, 301)
 
 	def test_register(self):
 		with app.test_client(self) as f:
@@ -39,52 +24,18 @@ class Users_TestCase(unittest.TestCase):
 			self.assertEqual(f.post('/api/v2/register',
 				json={"name":"fiona","username":"fifi","password":"2345","email":"fko@gmail.com"}).status_code, 301)
 
+	def test_register(self):
+		with app.test_client(self) as f:
+			response = f.post('/api/v2/register/')
+			self.assertEqual(f.post('/api/v2/register/',
+				json={"name":"","username":"fifi","password":"2345","email":"fko@gmail.com"}).status_code, 406)		
+
 	def test_logout(self):
 		res = app.test_client(self)
 		response= res.get('/api/v2/logout/', content_type='application/json')
 		self.assertEqual(response.status_code, 200)
 
-		
-		
-	def test_register_with_valid_email(self):
-		test_data = json.dump(
-			{"name":"fiona", 
-			"username":"fifi", 
-			"password":"fifi2345", 
-			"repeat_password":"fifi2345", 
-			"email":"fifi@gmail.com"})
-		response = self.app.post('api/v2/register/', Content-type='application/json')
-		self.assertEqual(response.status_code, 200)
 
-	def test_register_with_invalid_email(self):
-			test_data = json.dump(
-			{"name":"fiona", 
-			"username":"fifi", 
-			"password":"fifi2345", 
-			"repeat_password":"fifi2345", 
-			"email":"fi.com"})
-		response = self.app.post('api/v2/register/', Content-type='application/json')
-		self.assertEqual(response.status_code, 406)
-		
-	def test_register_with_empty_username(self):
-		test_data = json.dump(
-			{"name":"fiona", 
-			"username":"", 
-			"password":"fifi2345", 
-			"repeat_pasword":"fifi2345", 
-			"email":"i.com"})
-		response = self.app.post('api/v2/register/', Content-type='application/json')
-		self.assertEqual(response.status_code, 406)
-
-	def test_register_with_empty_name(self):
-		test_data = json.dump(
-			{"name":"", 
-			"username":"", 
-			"password":"fifi2345", 
-			"repeat_password":"fifi2345", 
-			"email":"fi.com"})
-		response = self.app.post('api/v2/register/', Content-type='application/json')
-		self.assertEqual(response.status_code, 406)	
 		
 
 if __name__ == '__main__':
