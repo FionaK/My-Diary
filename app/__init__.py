e10c23c466b56a2503289751d30b063eb17c0c40
Fiona K Owuor <fiona@localhost.localdomain>
2018-08-03 02:15:46 +0300

    update changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask import Blueprint, Flask
 flask_app = Flask(__name__)
 flask_app.secret_key = 'fifi'
 
-from users.views import users_blueprint
-from entries.views import entries_blueprint
-from route import home_blueprint
+from .users.views import users_blueprint
+from .entries.views import entries_blueprint
+from .route import home_blueprint
 
 flask_app.register_blueprint(users_blueprint)
 flask_app.register_blueprint(entries_blueprint)
